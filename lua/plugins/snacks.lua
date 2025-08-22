14d80524aa2b52db6f059d2ce5e8640cfdce0210
Ricardo Avila <ricardo.avila@empiricotx.com>
2025-08-22 02:35:07 -0400

    Update dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -53,22 +53,8 @@ return {
         sections = {
           { section = "header" },
           { section = "keys", gap = 1, padding = 1 },
-          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-          {
-            pane = 2,
-            icon = " ",
-            title = "Git Status",
-            section = "terminal",
-            enabled = function()
-              return Snacks.git.get_root() ~= nil
-            end,
-            cmd = "hub status --short --branch --renames",
-            height = 5,
-            padding = 1,
-            ttl = 5 * 60,
-            indent = 3,
-          },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 , limit = 10 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1, limit = 10 },
           { section = "startup" },
         },
       },
