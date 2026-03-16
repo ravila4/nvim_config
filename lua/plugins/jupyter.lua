5b16257b24839c7f9960febb77231b00a21bba46
Ricardo Avila <ricardo.avila@empiricotx.com>
2026-03-15 22:46:33 -0400

    Enable image.nvim window_overlap_clear to hide images behind floating windows

--- lua/plugins/jupyter.lua
+++ lua/plugins/jupyter.lua
@@ -116,7 +116,7 @@ return {
         max_height = nil,
         max_width_window_percentage = nil,
         max_height_window_percentage = 50,
-        window_overlap_clear_enabled = false,
+        window_overlap_clear_enabled = true,
         window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
       })
     end,
